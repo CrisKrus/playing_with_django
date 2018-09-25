0d70b029af877216a013d8350ee746aff49a9169
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-09-25 12:17:32 +0100

    Rename File -> GithubFile and read file content

--- code_testing/models.py
+++ code_testing/models.py
@@ -1,9 +1,10 @@
-import requests
+import base64
 
+import requests
 from django.db import models
 
 
-class File(models.Model):
+class GithubFile(models.Model):
     username = models.CharField(max_length=200)
     repository = models.CharField(max_length=300)
     path = models.CharField(max_length=1000)
@@ -11,8 +12,16 @@ class File(models.Model):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        request = requests.get('https://api.github.com/repos/Criskrus/juego-saber/readme')
+        # TODO: extract to github client
+        url = 'https://api.github.com/repos/' + \
+              self.username + '/' + \
+              self.repository + '/' + \
+              self.path
+        request = requests.get(url)
         self.__data = request.json()
 
     def type(self):
         return self.__data['type']
+
+    def content(self):
+        return base64.b64decode(self.__data['content'])
--- code_testing/tests.py
+++ code_testing/tests.py
@@ -1,19 +1,27 @@
 from django.test import TestCase
 
-from code_testing.models import File
+from code_testing.models import GithubFile
 
 
-class GithubFile(TestCase):
+# if file don't exists
+
+class GithubFileTest(TestCase):
     def test_read_file_content_from_github(self):
         """
-        get_content() returns the content of the file
+        content() returns the content of the file
         """
+        readme = GithubFile(username='criskrus',
+                            repository='juego-saber',
+                            path='readme')
+        # TODO this test is wrong
+        self.assertNotEquals(readme.content(), 'irrelevant')
 
     def test_create_file_from_github(self):
         """
         File(user, repo, path) returns a file with the data
         """
-        readme = File(username='criskrus',
-                      repository='juego-saber',
-                      path='readme')
+        readme = GithubFile(username='criskrus',
+                            repository='juego-saber',
+                            path='readme')
         self.assertEqual(readme.type(), "file")
+
